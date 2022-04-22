4ebe8c7875971911d92a3c3d217bec381b1deea1
Coder <coder.developer@cd.com>
2022-04-22 04:19:07 +0300

    get and list data from database

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,7 +14,10 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('admin.category.index');
+        $data= Category::all();
+        return view('admin.category.index',[
+            'data' => $data
+        ]);
     }
 
     /**
@@ -42,6 +45,7 @@ class CategoryController extends Controller
         $data ->description = $request->description;
         $data ->status = $request->status;
         $data->save();
+        return redirect('admin/category');
     }
 
     /**
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -14,8 +14,8 @@
 				</div>
 				<div class="col sm-6">
 					<ol class=" breadcrumb float-sm-right">
-						<li class="breadcrumb-item"><a href="#">HOME</a></li>
-						<li class="breadcrumb-item active">BLANK PAGE</li>
+						<li class="breadcrumb-item"><a href="/admin">HOME</a></li>
+						<li class="breadcrumb-item active">Category List</li>
 					</ol>
 				</div>
 			</div>
@@ -23,28 +23,61 @@
 	</section>
 
 	<!-- MAİN CONTENT-->
-	<section class=" content">
+	<section class=" content-list">
 		<!-- Default box -->
 		<div class="card">
-			<!-- card header -->
 			<div class="card-header">
-
+				<h3 class="card-title">Category List</h3>
 			</div>
-			<!--/.card header -->
 
-			<!-- card body -->
-			<div class="card-body">
-				ARE YOU READYYYYYYY
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-md-6 text-center mb-5">
+						<h2 class="heading-section">Table #03</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<h4 class="text-center mb-4">Create Your Domain Name</h4>
+						<div class="table-wrap">
+							<table class="table">
+								<thead class="thead-primary">
+									<tr>
+										<th>ID</th>
+										<th>Title</th>
+										<th>Keywords</th>
+										<th>Description</th>
+										<th>Image</th>
+										<th>Status</th>
+										<th>Edit</th>
+										<th>Delete</th>
+										<th>Show</th>
+										
+									</tr>
+								</thead>
+								<tbody>
+									@foreach($data as $rs)
+									<tr>
+										<td>{{$rs->id}}</td>
+										<td>{{$rs->title}}</td>
+										<td>{{$rs->keywords}}</td>
+										<td>{{$rs->description}}</td>
+										<td>{{$rs->image}}</td>
+										<td>{{$rs->status}}</td>
+										<td><a href="/admin/category/edit{{$rs->id}}" class="btn btn-outline-success">EDİT</a></td>
+										<td><a href="/admin/category/delete{{$rs->id}}" class="btn btn-outline-danger">DELETE</a</td>
+										<td><a href="/admin/category/show{{$rs->id}}" class="btn btn-outline-primary">SHOW</a</td>
+									</tr>	
+									
+									@endforeach								
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
 			</div>
-			<!-- /.card body -->
-
-			<!-- card footer -->
-			<div class="card-footer">
-				FOOTER
-			</div>
-			<!--/. card footer -->
+			<!--/.card -->
 		</div>
-		<!--/.card -->
 	</section>
 
 
